5b4c4b38bf500e3e1b8956dc1812ed66eeb959d8
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-11-09 21:10:12 +0700

    add example code

---
+++ backend/models/user.js
@@ -0,0 +1,12 @@
+/**
+ * TODO: Database needed
+ */
+const mongoose = require('mongoose');
+const userSchema = mongoose.Schema({
+   name: { type: String, required: true },
+   email: { type: String, unique: true, lowercase: true, trim: true },
+   password: { type: String, required: true },
+   role: { type: String, default: 'user' },
+   date: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model("User", userSchema);
