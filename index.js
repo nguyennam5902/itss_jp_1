11839930671df2dc774816b42af2e382465a052c
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-11-17 22:13:42 +0700

    add module

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const db = require('./backend/database/db');
+const wordAPI = require('./backend/routes/api/word');
 const app = express();
+const KuromojiAnalyzer = require("kuroshiro-analyzer-kuromoji");
+const Kuroshiro = require("kuroshiro")
+const kuroshiro = new Kuroshiro();
+
+kuroshiro.init(new KuromojiAnalyzer()).then(console.log('APP STARTED'));
+db.connect();
 
 app.use(express.json());
 app.set('port', 3000);
@@ -10,9 +18,24 @@ app.get('/', (req, res) => {
 });
 app.post('/api/login', require('./backend/routes/api/login'));
 
+// TODO
+app.get('/api/search/:word', async (req, res) => {
+   const word = String(req.params.word);
+   const romajiWord = await kuroshiro.convert(word, { to: "romaji" });
+   const result = await wordAPI.search(romajiWord);
+   res.send({
+      data: result,
+      meta:{
+         'code': 200,
+         'message': 'OK'
+      }
+   });
+})
+
+// Do later
 app.post('/login', (req, res) => {
-   let username = req.body.username;
-   let password = req.body.password;
+   const username = req.body.username;
+   const password = req.body.password;
    if (username === 'admin' && password === '123456') {
       // 登录成功，返回token
       res.send({
