d4409e808ebee597e6294225166d6736d1dbd35c
namhhbg0001 <nam.ng205103@sis.hust.edu.vn>
2023-11-21 07:52:28 +0700

    search bar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -9,6 +9,14 @@ const SearchBar = ({onSearch}) => {
             className="border p-2 rounded focus:outline-none focus:ring focus:border-blue-300"
             onChange={(e) => onSearch(e.target.value)}
           />
+          <select
+            value={selectedOption}
+            onChange={(e) => setSelectedOption(e.target.value)}
+          >
+
+            <option value="option1">Type 1</option>
+            <option value="option2">Type 2</option>
+          </select>
           <button className="text-white p-2 rounded"><img src="./assets/icons/search.png" alt = "Search Button"/></button>
         </div>
       );
--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import SearchBar from '../components/SearchBar'
 
 const Search = () => {
   return (
-    <div>
-      <h1>Search page</h1>
+    <div className="w-full">
+        <div><img src="./assets/icons/home.png" alt = ""></img></div>
+        <button class="bg-neutral-200 font-bold py-2 px-4 rounded text-sm ml-5 mt-5" onClick={backAction}>
+            Back
+        </button>
+        <div className = "m-10 flex rounded-2xl justify-center items-center">
+          <h1 className="text-2xl font-bold">
+            <SearchBar/>
+          </h1>
+        </div>
+        <div className="grid grid-cols-2 gap-4 m-10 h-[65vh] overflow-y-auto">
+            <Word/>
+            <Word/>
+            <Word/>
+            <Word/>
+            <Word/>
+            <Word/>
+            <Word/>
+            <Word/>
+            <Word/>
+        </div>
+        <div className="font-bold text-red-500 text-sm">Completed percentage: 80%</div>
     </div>
   )
 }
