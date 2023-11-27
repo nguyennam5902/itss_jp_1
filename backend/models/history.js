c1c650bc31b11883de6aa96ac5f105cc25f2dd50
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-11-27 18:27:29 +0700

    fix history model

--- backend/models/history.js
+++ backend/models/history.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const historySchema = new mongoose.Schema({
    user_id: mongoose.Types.ObjectId,
-   vocab_looked_up: mongoose.Types.ObjectId,
-   vocab_marked: mongoose.Types.ObjectId
+   vocab_looked_up: mongoose.Types.ObjectId
 });
 const History = mongoose.model('history', historySchema);
 module.exports = History;
