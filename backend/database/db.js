de8d2897d9ebd2009a8c488c22b117b5f9e81a32
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-11-28 16:53:16 +0700

    add link mongo on cloud

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
    try {
-      await mongoose.connect('mongodb://127.0.0.1:27017/itss');
+      await mongoose.connect('mongodb+srv://test:test@itss.nfhvwzs.mongodb.net/?retryWrites=true&w=majority');
       console.log('Connect MongoDB Successfully!!');
    } catch (error) {
       console.log('Connect MongoDB failure: ' + error.message);
