c517319c332683d36151b20c81b3bee3176df594
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-12-02 09:53:44 +0700

    fix API search word

--- backend/routes/api/word.js
+++ backend/routes/api/word.js
@@ -1,18 +1,13 @@
 const Vocab = require('../../models/vocab')
 /**
- * Find all words start with `anotherWord`
+ * Find all words start with `searchWord`
  * @param {String} searchWord 
  * @returns 
  */
 const search = async function (searchWord) {
    const regex = new RegExp(searchWord, 'i');
    const words = await Vocab.find({ romaji: regex }).limit(10).exec();
-   console.log(words);
-   const result = [];
-   for (let i = 0; i < words.length; i++) {
-      result.push(words[i]._id.toString());
-   }
-   return result;
+   return words;
 }
 const all = async (req, res) => {
    const words = await Vocab.find({}).exec();
