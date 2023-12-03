cb6b6c275698c23613d5f7f90698683356a29a82
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-12-03 15:14:10 +0700

    remove comments in code

--- backend/routes/api/word.js
+++ backend/routes/api/word.js
@@ -8,13 +8,6 @@ const search = async function (searchWord) {
    const regex = new RegExp(searchWord, 'i');
    const words = await Vocab.find({ romaji: regex }).limit(10).exec();
    return words;
-   
-   console.log(words);
-   // const result = [];
-   // for (let i = 0; i < words.length; i++) {
-   //    result.push(words[i]._id.toString());
-   // }
-   // return result;
 }
 const all = async (req, res) => {
    const words = await Vocab.find({}).exec();
