409edc35f29e4d6a0533bd876b66afbd8f4a9a3c
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-12-03 15:13:05 +0700

    remove comments

--- backend/routes/api/topic.js
+++ backend/routes/api/topic.js
@@ -9,21 +9,9 @@ const topic = async function (searchWord) {
    const regex = new RegExp(searchWord, 'i');
    const topics = await Topic.find({ topic_name: regex }).limit(10).exec();
    return topics;
-   // console.log(topics);
-   // const result = [];
-   // for (let i = 0; i < topics.length; i++) {
-   //    result.push(topics[i]._id.toString());
-   // }
-   // return result;
 }
 const detail = async (topicID) => {
    const words = await Vocab.find({ topic_id: topicID }).exec();
    return words;
-   // const result = [];
-   // const words = (await Vocab.find({ topic_id: topicID }).exec());
-   // for (let i = 0; i < words.length; i++) {
-   //    result.push(words[i]._id.toString());
-   // }
-   // return result;
 }
 module.exports = { topic, detail };
